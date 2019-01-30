022b927494f244913e5db2f0280150d5554a0a6c
Lajos Bodnar <azazamaz7@gmail.com>
2019-01-30 16:58:27 +0100

    Add onBackPressed event.

--- app/src/main/java/home/lali/darts/MainActivity.java
+++ app/src/main/java/home/lali/darts/MainActivity.java
@@ -10,57 +10,38 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button newgameBtn;
-    private Button exitBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        newgameBtn = findViewById(R.id.newGame_btn);
-        exitBtn = findViewById(R.id.exit_btn);
+        Button newGameBtn;
 
-        newgameBtn.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view){
-                Intent intent = new Intent(MainActivity.this, NewGameActivity.class);
-                Bundle extras = new Bundle();
-                extras.putString("EXTRA_MODE1", "501");
-                extras.putString("EXTRA_MODE2", "301");
-                intent.putExtras(extras);
-                startActivity(intent);
-            }
-        });
+        newGameBtn = findViewById(R.id.newGame_btn);
 
-        exitBtn.setOnClickListener(new View.OnClickListener() {
+        newGameBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                AlertDialog dialog = exitDialog();
-                dialog.setIcon(R.drawable.launcher);
-                dialog.show();
+                Intent intent = new Intent(MainActivity.this, NewGameActivity.class);
+                startActivity(intent);
             }
         });
     }
 
-    private AlertDialog exitDialog(){
-        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
-        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {
-                finish();
-            }
-        });
-
-        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialogInterface, int i) {}
-        });
-
-        builder.setTitle("Exit");
-        builder.setMessage("You will exit from the application. Are you sure?");
-
-        AlertDialog dialog = builder.create();
-        return  dialog;
+    @Override
+    public void onBackPressed() {
+        new AlertDialog.Builder(this)
+                .setIcon(R.drawable.launcher)
+                .setTitle("Exit")
+                .setMessage("You will exit from the application. Are you sure?")
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        finish();
+                    }
+                })
+                .setNegativeButton("No", null)
+                .show();
     }
 }
