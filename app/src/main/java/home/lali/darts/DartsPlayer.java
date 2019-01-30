81de610de4ae3c6b30df416a5ea16a2eda91a112
Lajos Bodnar <azazamaz7@gmail.com>
2019-01-30 16:55:38 +0100

    Added: legStart get, set Removed: setW

--- app/src/main/java/home/lali/darts/DartsPlayer.java
+++ app/src/main/java/home/lali/darts/DartsPlayer.java
@@ -1,91 +1,77 @@
 package home.lali.darts;
 
-/**
- * Created by Lali on 2018. 03. 26..
- */
-
-public class DartsPlayer {
+class DartsPlayer {
     private String playerName;
     private int score;
     private int legW;
-    private int setW;
     private int lastScore;
-    private double avg3dart;
     private double legAvg;
     private double matchAvg;
+    private boolean legStart;
 
-    public DartsPlayer(){
+    DartsPlayer(){
         playerName = "Darts Player";
         score = 0;
         legW = 0;
-        setW = 0;
-        avg3dart = 0;
         legAvg = 0;
         matchAvg = 0;
         lastScore = 0;
+        legStart = false;
     }
 
-    public String getPlayerName(){
+    String getPlayerName(){
         return playerName;
     }
 
-    public void setPlayerName(String playerName){
+    void setPlayerName(String playerName){
         this.playerName = playerName;
     }
 
-    public int getScore(){
+    int getScore(){
         return score;
     }
 
-    public void setScore(int score){
+    void setScore(int score){
         this.score = score;
     }
 
-    public int getLegW () {
+    int getLegW () {
         return legW;
     }
 
-    public void setLegW(int legW){
+    void setLegW(int legW){
         this.legW = legW;
     }
 
-    public int getSetW(){
-        return setW;
-    }
-
-    public void setSetW(int setW){
-        this.setW = setW;
-    }
-
-    public double getAvg3dart(){
-        return avg3dart;
-    }
-
-    public void setAvg3dart(double avg3){
-        this.avg3dart = avg3;
-    }
-
-    public double getLegAvg(){
+    double getLegAvg(){
         return legAvg;
     }
 
-    public void setLegAvg(double legAvg){
+    void setLegAvg(double legAvg){
         this.legAvg = legAvg;
     }
 
-    public double getMatchAvg(){
+    double getMatchAvg(){
         return matchAvg;
     }
 
-    public void setMatchAvg(double matchAvg){
+    void setMatchAvg(double matchAvg){
         this.matchAvg = matchAvg;
     }
 
-    public int getLastScore(){
+    int getLastScore(){
         return lastScore;
     }
 
-    public void setLastScore(int lastScore){
+    void setLastScore(int lastScore){
         this.lastScore = lastScore;
+    }
+
+    boolean getLegStart() {
+        return legStart;
+    }
+
+    void setLegStart(boolean legStart){
+        this.legStart = legStart;
     }
 }
