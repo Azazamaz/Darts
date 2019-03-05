3755cf79b5b911a8622eca343c31adf1e8e14bbf
Lajos Bodnar <azazamaz7@gmail.com>
2019-03-05 12:33:27 +0100

    User authentication for OnlineGame.

--- app/src/main/java/home/lali/darts/MainActivity.java
+++ app/src/main/java/home/lali/darts/MainActivity.java
@@ -5,21 +5,38 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.auth.api.Auth;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+
 public class MainActivity extends AppCompatActivity {
 
+    private static final int RC_SIGN_IN = 1;
+
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        firebaseAuth = FirebaseAuth.getInstance();
 
         Button newGameBtn;
         Button newOnlineGameBtn;
@@ -47,12 +64,16 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 if (isOnline()) {
-                    //TODO: Bejelentkezés, flag
-                    Intent intent = new Intent(MainActivity.this, NewGameActivity.class);
-                    intent.putExtra("ONLINE_GAME", true);
+                    FirebaseUser user = firebaseAuth.getCurrentUser();
+                    if (user != null) {
+                        Intent intent = new Intent(MainActivity.this, NewGameActivity.class);
+                        intent.putExtra("ONLINE_GAME", true);
+                        intent.putExtra("CURRENT_USER", user.getDisplayName());
 
-                    startActivity(intent);
-
+                        startActivity(intent);
+                    } else {
+                        useSignIn().show();
+                    }
                 } else {
                     noInternet().show();
                 }
@@ -66,7 +87,7 @@ public class MainActivity extends AppCompatActivity {
                     Intent intent = new Intent(MainActivity.this, LiveMatchActivity.class);
                     startActivity(intent);
                 } else {
-                    Toast.makeText(MainActivity.this, "Need internet", Toast.LENGTH_LONG).show();
+                    noInternet(LiveMatchActivity.class).show();
                 }
             }
         });
@@ -86,10 +107,52 @@ public class MainActivity extends AppCompatActivity {
                     Intent intent = new Intent(MainActivity.this, GlobalStatActivity.class);
                     startActivity(intent);
                 } else {
-                    Toast.makeText(MainActivity.this, "Need internet", Toast.LENGTH_LONG).show();
+                    noInternet(GlobalStatActivity.class).show();
                 }
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.sign_in_menu:
+                if (isOnline()) {
+                    startActivityForResult(
+                            AuthUI.getInstance()
+                                    .createSignInIntentBuilder()
+                                    .setAvailableProviders(Arrays.asList(new AuthUI.IdpConfig.EmailBuilder().build()))
+                                    .build(),
+                            RC_SIGN_IN);
+                } else {
+                    Toast.makeText(MainActivity.this, "Lost connection!", Toast.LENGTH_LONG).show();
+                }
+            case R.id.sign_out_menu:
+                AuthUI.getInstance().signOut(MainActivity.this);
+                Toast.makeText(MainActivity.this, "Signed out!", Toast.LENGTH_LONG).show();
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(MainActivity.this, "Signed in!", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(MainActivity.this, "Sign in canceled!", Toast.LENGTH_LONG).show();
+            }
+        }
     }
 
     @Override
@@ -129,11 +192,16 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
                         if (isOnline()) {
-                            //TODO: Bejelentkezés, intent
-                            Intent intent = new Intent(MainActivity.this, NewGameActivity.class);
-                            intent.putExtra("ONLINE_GAME", true);
+                            FirebaseUser user = firebaseAuth.getCurrentUser();
+                            if (user != null) {
+                                Intent intent = new Intent(MainActivity.this, NewGameActivity.class);
+                                intent.putExtra("ONLINE_GAME", true);
+                                intent.putExtra("CURRENT_USER", user.getDisplayName());
 
-                            startActivity(intent);
+                                startActivity(intent);
+                            } else {
+                                useSignIn().show();
+                            }
                         } else {
                             noInternet().show();
                         }
@@ -149,4 +217,45 @@ public class MainActivity extends AppCompatActivity {
         return builder.create();
     }
 
+    private AlertDialog noInternet(final Class<?> cls) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+
+        builder.setIcon(R.mipmap.offline_icon)
+                .setTitle("No internet")
+                .setMessage("Lost internet connection")
+                .setPositiveButton("Try again", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        if (isOnline()) {
+                            Intent intent = new Intent(MainActivity.this, cls);
+                            startActivity(intent);
+                        } else {
+                            noInternet(cls).show();
+                        }
+                    }
+                })
+                .setNegativeButton("Back", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+
+        return builder.create();
+    }
+
+    private AlertDialog useSignIn() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+
+        builder.setTitle("Sign in")
+                .setMessage("Use \'Sign in\' from menubar and try again!")
+                .setNeutralButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+
+        return builder.create();
+    }
 }
